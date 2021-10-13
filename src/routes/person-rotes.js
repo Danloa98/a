1684493c6e2703100aee8e00e2cb52c695c49410
daniel <daniedu998@gmail.com>
2021-10-13 17:26:12 -0600

    First commit

---
+++ src/routes/person-rotes.js
@@ -0,0 +1,77 @@
+const { Router } = require('express');
+const router = Router();
+const BD = require('../config/configbd');
+
+//READ
+router.get('/getUsers', async (req, res) => {
+    sql = "SELECT * FROM USUARIO";
+
+    let result = await BD.Open(sql, [], false);
+    Users = [];
+
+    result.rows.map(user => {
+        let userSchema = {
+            "ID": user[0]
+        }
+
+        Users.push(userSchema);
+    })
+
+    res.json(Users);
+})
+
+//CREATE
+
+router.post('/addUser', async (req, res) => {
+    const { username, firstname, lastname } = req.body;
+
+    sql = "insert into person(username,firstname,lastname) values (:username,:firstname,:lastname)";
+
+    await BD.Open(sql, [username, firstname, lastname], true);
+
+    res.status(200).json({
+        "username": username,
+        "firstname": firstname,
+        "lastname": lastname
+    })
+})
+
+//UPDATE
+router.put("/updateUser", async (req, res) => {
+    const { codu, username, firstname, lastname } = req.body;
+
+    sql = "update person set username=:username, firstname=:firstname, lastname=:lastname where codu=:codu";
+
+    await BD.Open(sql, [username, firstname, lastname,codu], true);
+
+    res.status(200).json({
+        "codu": codu,
+        "username": username,
+        "firstname": firstname,
+        "lastname": lastname
+    })
+
+})
+
+
+//DELETE
+router.delete("/deleteUser/:codu", async (req, res) => {
+    const { codu } = req.params;
+
+    sql = "update person set state=0 where codu=:codu";
+
+    await BD.Open(sql, [codu], true);
+
+    res.json({ "msg": "Usuario Eliminado" })
+})
+
+
+module.exports = router;
+
+
+router.get('/',(req,res)=>{
+    res.status(200).json({
+
+        msg:"Todo ok"
+    })
+})
